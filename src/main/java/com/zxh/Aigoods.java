7e49ed3ab86d28135ad3e5ce765af98469ba39b1
zxh <18879234422@163.com>
2021-12-08 17:49:45 +0800

    第5次 de

--- src/main/java/com/zxh/Aigoods.java
+++ src/main/java/com/zxh/Aigoods.java
@@ -3,6 +3,7 @@ package main.java.com.zxh;
 class aigoods {
     public static void main(String[] args) {
         System.out.println("aigoods1");
-        System.out.println("aigoods");
+        System.out.println("aigoods2");
+        System.out.println("aigoods3");
     }
 }
