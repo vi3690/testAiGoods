17e17eb02b306d99e16c196d6856b6957c3f52b4
zxh <18879234422@163.com>
2021-12-08 16:17:19 +0800

    第4次 de

--- src/main/java/com/zxh/test1.java
+++ src/main/java/com/zxh/test1.java
@@ -3,8 +3,8 @@ package main.java.com.zxh;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("a");
-        System.out.println("a");
-        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
         System.out.println("b");
         System.out.println("c");
         System.out.println("d");
