1aeb55ba9ed9c49e4e6ecf8d5c71cb71098186ff
zxh <18879234422@163.com>
2021-12-08 16:21:31 +0800

    第4次 de

--- src/main/java/com/zxh/test1.java
+++ src/main/java/com/zxh/test1.java
@@ -2,16 +2,14 @@ package main.java.com.zxh;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("a");
+        System.out.println("A");
         System.out.println("b");
         System.out.println("c");
         System.out.println("b");
-        System.out.println("c");
-        System.out.println("D");
         System.out.println("E");
         System.out.println("f");
         System.out.println("g");
         System.out.println("h");
-
+        System.out.println("i");
     }
 }
