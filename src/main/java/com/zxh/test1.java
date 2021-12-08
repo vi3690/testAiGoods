ade6c29dad080d00805475abbf78f07e415e81b5
zxh <18879234422@163.com>
2021-12-08 16:06:59 +0800

    第二次 ab

--- src/main/java/com/zxh/test1.java
+++ src/main/java/com/zxh/test1.java
@@ -3,5 +3,7 @@ package main.java.com.zxh;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("helle");
+        System.out.println("a");
+        System.out.println("b");
     }
 }
