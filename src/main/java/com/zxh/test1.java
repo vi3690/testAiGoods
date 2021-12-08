fd558b3626e85db887ed28506dd184194019a1ff
zxh <18879234422@163.com>
2021-12-08 16:11:39 +0800

    第4次 de

--- src/main/java/com/zxh/test1.java
+++ src/main/java/com/zxh/test1.java
@@ -2,8 +2,9 @@ package main.java.com.zxh;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("helle24");
+        System.out.println("a");
         System.out.println("b");
         System.out.println("c");
+        System.out.println("f");
     }
 }
