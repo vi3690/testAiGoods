2b3544a6ed89f05af79bf0eb1be658507b7039f2
vi3690 <42235087+vi3690@users.noreply.github.com>
2021-12-08 17:21:07 +0800

    Update test1.java

--- src/main/java/com/zxh/test1.java
+++ src/main/java/com/zxh/test1.java
@@ -2,9 +2,9 @@ package main.java.com.zxh;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("a");
-        System.out.println("b");
-        System.out.println("c");
+        System.out.println("a1");
+        System.out.println("b2");
+        System.out.println("c3");
         System.out.println("b");
         System.out.println("c");
         System.out.println("D");
