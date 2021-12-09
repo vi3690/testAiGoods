0467e823350e0fd00b268ddd7480e96a7cee7531
vi3690 <42235087+vi3690@users.noreply.github.com>
2021-12-09 11:33:44 +0800

    Update auditcontroller.java

--- src/main/java/com/zxh/auditcontroller.java
+++ src/main/java/com/zxh/auditcontroller.java
@@ -3,5 +3,8 @@ package main.java.com.zxh;
 public class auditcontroller {
     public static void main(String[] args) {
         System.out.println("***");
+        System.out.println("*2*");
+        System.out.println("*3*");
+        System.out.println("*4*");
     }
 }
