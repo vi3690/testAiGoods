2e9771f7264ba2556b8157056e5f836443628ba5
zxh <18879234422@163.com>
2022-06-22 17:22:15 +0800

    提交221722

--- src/main/java/com/zxh/auditServe.java
+++ src/main/java/com/zxh/auditServe.java
@@ -2,6 +2,15 @@ package main.java.com.zxh;
 
 public class auditServe {
 
+    public ClaimDetail convert(StatusInfo.Sljgmx f) {
+        ClaimDetail r = new ClaimDetail();
+        r.setCode(f.fpdm);
+        r.setQuantity(f.fs);
+        r.setBeginNumber(f.qshm);
+        r.setEndNumber(f.zzhm);
+        return r;
+    }
+
     @ApiOperation("撤销申领")
     @DeleteMapping("/{diskId}/{invoiceType}/{applySn}")
     @CheckTerminal
@@ -10,5 +19,17 @@ public class auditServe {
         Terminal terminal = deviceService.getDbDevice(diskId);
         return invoiceApplyService.deleteClaim(terminal, invoiceType, applySn);
     }
-
+    /**
+     * 撤销申领
+     * @param diskId 税控盘盘号
+     * @param invoiceType 发票类型代码
+     * @param applySn 申领编号
+     * @return
+     */
+    @ApiOperation("撤销申领")
+    @DeleteMapping("/{diskId}/{invoiceType}/{applySn}")
+    public ErrorDetails deleteClaim(@PathVariable String diskId, @PathVariable String invoiceType,
+                                    @PathVariable String applySn) {
+        return claimApiService.deleteClaim(diskId, invoiceType, applySn);
+    }
 }
