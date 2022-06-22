c9e094d308bae90e29db79c9a0a210a839b987a2
zxh <18879234422@163.com>
2022-06-22 17:19:18 +0800

    提交221719

--- src/main/java/com/zxh/auditServe.java
+++ src/main/java/com/zxh/auditServe.java
@@ -2,6 +2,13 @@ package main.java.com.zxh;
 
 public class auditServe {
 
-
+    @ApiOperation("撤销申领")
+    @DeleteMapping("/{diskId}/{invoiceType}/{applySn}")
+    @CheckTerminal
+    public ErrorDetails deleteClaim(Authentication authentication, @PathVariable String diskId, @PathVariable String invoiceType,
+                                    @PathVariable String applySn) {
+        Terminal terminal = deviceService.getDbDevice(diskId);
+        return invoiceApplyService.deleteClaim(terminal, invoiceType, applySn);
+    }
 
 }
